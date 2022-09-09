5fb1f11b8b36c1a6b8ffe5a9ee821a3b8eeae26e
Hug0-Drelon <hugo.drelon@gmail.com>
2022-09-09 16:14:53 +0200

    Add comment in tests.

--- tests/data/get_languages_list.php
+++ tests/data/get_languages_list.php
@@ -14,13 +14,18 @@ $array = $array;
 
 $args = ['fields' => 'slug'];
 
+// With 'fields' key set clearly.
 assertType('array<int, string>', $model->get_languages_list(['fields' => 'slug']));
 
+// With 'fields' key set in a variable.
 assertType('array<int, string>', $model->get_languages_list($args));
 
+// With a variable containing unkown data.
 assertType('array<int, mixed>', $model->get_languages_list($array));
 
+// With array_merge() result passed as parameter.
 assertType('array<int, mixed>', $model->get_languages_list(array_merge($array, ['fields' => 'slug'])));
 
+// With 'fields' key set on top of variable containing unkown data.
 $array['fields'] = 'slug';
 assertType('array<int, string>', $model->get_languages_list($array));
