090dfcf485d7e4e89c323f29cc6f89c7bcf11ff8
Hugo Drelon <69580439+Hug0-Drelon@users.noreply.github.com>
2022-09-09 16:24:40 +0200

    Add tests for get_languages_list() return types (#6)
    * Add tests for PLL_Model::get_languages_list() return type.
    
    * Add a test.
    
    * Add comment in tests.
    
    * Travis please do your job.

---
+++ tests/data/get_languages_list.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPSyntex\Polylang\PHPStan\Tests;
+
+use function PHPStan\Testing\assertType;
+
+/** @var \PLL_Model */
+$model = $model;
+
+/** @var array */
+$array = $array;
+
+$args = ['fields' => 'slug'];
+
+// With 'fields' key set explicitly.
+assertType('array<int, string>', $model->get_languages_list(['fields' => 'slug']));
+
+// With 'fields' key set in a variable.
+assertType('array<int, string>', $model->get_languages_list($args));
+
+// With a variable containing unkown data.
+assertType('array<int, mixed>', $model->get_languages_list($array));
+
+// With array_merge() result passed as parameter.
+assertType('array<int, mixed>', $model->get_languages_list(array_merge($array, ['fields' => 'slug'])));
+
+// With 'fields' key set on top of variable containing unkown data.
+$array['fields'] = 'slug';
+assertType('array<int, string>', $model->get_languages_list($array));
