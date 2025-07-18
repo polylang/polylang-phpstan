5162dd4409fe46a74432e9a1628d7d6b14a376af
Grégory Viguier <Screenfeed@users.noreply.github.com>
2025-07-18 15:32:09 +0200

    Create extension for `Model\Languages::get_list()` (#14)

---
+++ src/ModelLanguagesGetListDynamicMethodReturnTypeExtension.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * Dynamic return type for `Model\Languages::get_list()`.
+ */
+
+declare(strict_types=1);
+
+namespace WPSyntex\Polylang\PHPStan;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\NullType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use PLL_Language;
+use WP_Syntex\Polylang\Model\Languages;
+
+class ModelLanguagesGetListDynamicMethodReturnTypeExtension implements DynamicMethodReturnTypeExtension {
+	public function getClass(): string {
+		return Languages::class;
+	}
+
+	public function isMethodSupported( MethodReflection $methodReflection ): bool {
+		return $methodReflection->getName() === 'get_list';
+	}
+
+	public function getTypeFromMethodCall( MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope ): Type {
+		$args = $methodCall->getArgs();
+
+		if ( count( $args ) === 0 ) {
+			// Called without arguments.
+			return new ArrayType( new IntegerType(), new ObjectType( PLL_Language::class ) );
+		}
+
+		$argumentType = $scope->getType( $args[0]->value );
+
+		if ( ! $argumentType->isArray()->yes() ) {
+			// Called with an argument that is not an array-like.
+			return new ArrayType( new IntegerType(), new ObjectType( PLL_Language::class ) );
+		}
+
+		if ( $argumentType instanceof IntersectionType && $argumentType->isArray()->yes() ) {
+			$fieldsInst = new ConstantStringType( 'fields' );
+
+			foreach( $argumentType->getTypes() as $type ) {
+				if ( ! $type->hasOffsetValueType( $fieldsInst )->yes() ) {
+					continue;
+				}
+
+				$fieldsType = $type->getOffsetValueType( $fieldsInst );
+
+				if ( ! $fieldsType instanceof ConstantStringType ) {
+					// The 'field' argument is not a string.
+					return new ArrayType( new IntegerType(), new ObjectType( PLL_Language::class ) );
+				}
+
+				$fieldsValue = $fieldsType->getValue();
+				break;
+			}
+		}
+
+		if ( ! isset( $fieldsValue ) && $argumentType instanceof ArrayType ) {
+			$argumentKeys = $argumentType->getKeysArray();
+
+			if ( $argumentKeys instanceof ConstantArrayType ) {
+				$argumentKeysTypes = $argumentKeys->getValueTypes();
+
+				if( empty( $argumentKeysTypes ) ) {
+					// An empty array is passed as parameter.
+					return new ArrayType( new IntegerType(), new ObjectType( PLL_Language::class ) );
+				}
+
+				foreach ( $argumentKeys->getValueTypes() as $index => $key ) {
+					if ( $key->getValue() !== 'fields' ) {
+						continue;
+					}
+
+					$fieldsType = $argumentType->getValuesArray()->getValueTypes()[ $index ];
+
+					if ( ! $fieldsType instanceof ConstantStringType ) {
+						// The 'field' argument is not a string.
+						return new ArrayType( new IntegerType(), new ObjectType( PLL_Language::class ) );
+					}
+
+					$fieldsValue = $fieldsType->getValue();
+					break;
+				}
+			}
+		}
+
+		if ( empty( $fieldsValue ) ) {
+			// Without 'fields' argument, or empty value.
+			return new ArrayType( new IntegerType(), new MixedType() );
+		}
+
+		switch ( $fieldsValue ) {
+			case 'term_id':
+			case 'term_group':
+			case 'term_taxonomy_id':
+			case 'count':
+			case 'tl_term_id':
+			case 'tl_term_taxonomy_id':
+			case 'tl_count':
+			case 'is_rtl':
+			case 'mo_id':
+				return new ArrayType( new IntegerType(), new IntegerType() );
+			case 'name':
+			case 'slug':
+			case 'locale':
+			case 'w3c':
+			case 'flag_code':
+				return new ArrayType( new IntegerType(), new StringType() );
+			case 'page_on_front':
+			case 'page_for_posts':
+				return new ArrayType( new IntegerType(), new UnionType( [ new IntegerType(), new NullType() ] ) );
+			case 'facebook':
+			case 'home_ur':
+			case 'search_url':
+			case 'host':
+			case 'flag_url':
+			case 'flag':
+			case 'custom_flag_url':
+			case 'custom_flag':
+				return new ArrayType( new IntegerType(), new UnionType( [ new StringType(), new NullType() ] ) );
+			default:
+				// Everything exploded.
+				return new NullType();
+		}
+	}
+}
